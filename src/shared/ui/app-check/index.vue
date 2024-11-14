6f3c9eb67b363228364dc6358aebc732f6e690b2
whoamizy <whoamizy@yandex.ru>
2024-11-14 23:19:03 +0400

    fix

--- src/shared/ui/app-check/index.vue
+++ src/shared/ui/app-check/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div
     class="inline-flex justify-start bg-[#DCDCDC] rounded-full w-8 h-4 p-[1px] cursor-pointer"
-    :class="{ '!bg-[#316FEE] !justify-end': isChecked, 'w-12 h-6': big }"
+    :class="{ '!bg-[#316FEE] !justify-end': isChecked, '!w-12 !h-6': big }"
     @click="emits('onCheck')"
   >
-    <div class="bg-white rounded-full w-[14px] h-[14px]" :class="{ 'w-[22px] h-[22px]': big }" />
+    <div class="bg-white rounded-full w-[14px] h-[14px]" :class="{ '!w-[22px] !h-[22px]': big }" />
   </div>
 </template>
 
